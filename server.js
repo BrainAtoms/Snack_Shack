518b2cffaa7f45795d9595cd4867a23328f9398b
Brain Atoms <badams6453@gmail.com>
2023-05-01 21:30:06 -0400

    added to seed function to seed file and created json files for recipe and user data

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-// const routes = require("./controllers");
+const routes = require("./controllers");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -20,10 +20,10 @@ const sess = {
 
 app.use(session(sess));
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
