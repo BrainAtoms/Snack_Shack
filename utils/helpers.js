0c6239f1f9577ca32a8cc577e5511e5dd6e35503
thomasjshea <thomasjshea89@gmail.com>
2023-05-02 20:08:39 -0500

    updated homeRoutes.js, helpers.js files

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+module.exports = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+
+    format_date: (date) => {
+      return date.toLocaleDateString()
+    }
+  };
+  
