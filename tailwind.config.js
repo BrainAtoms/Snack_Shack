e2b2a5741f69d45289ee0b7b1bfe71e6c29bf57d
thomasjshea <thomasjshea89@gmail.com>
2023-05-08 17:32:05 -0500

    updated styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-const colors = require('tailwind/colors')
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./views/**/*.handlebars"],
   theme: {
-    
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
       black: colors.black,
       white: colors.white,
-      gray: colors.gray,
+      gray: colors.neutral,
       indigo: colors.indigo,
       red: colors.rose,
       yellow: colors.amber,
